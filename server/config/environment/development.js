f589726d8005556b7b9405bcd4a41dfbcd5d1b1b
Jake Roby <jroby@extendmed.com>
2015-07-01 14:16:35 -0500

    package name update

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/mtgoroi-dev'
+    uri: 'mongodb://localhost/extendmedBoilerplate-dev'
   },
 
   seedDB: true
--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -5,6 +5,6 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/mtgoroi-test'
+    uri: 'mongodb://localhost/extendmedBoilerplate-test'
   }
 };
